4f3b209c9a750b79031f177d933f7653d0e1829e
kOFReadie <60425965+kOFReadie@users.noreply.github.com>
2020-06-28 14:45:07 +0100

    Added custom domain

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ window.addEventListener('load', (event) =>
 function indexMembers()
 {
     var request = new XMLHttpRequest();
-    request.open("GET", "/kOFWebsite/members/pages.json", false);
+    request.open("GET", "/members/pages.json", false);
     request.send(null);
     var json = JSON.parse(request.responseText);
     json.forEach(element =>
     {
         var a = document.createElement("a");
         a.innerHTML = element.DisplayName;
-        a.href = "/kOFWebsite/members/" + element.DisplayName.toString().replace(' ', '').toLowerCase();
+        a.href = "/members/" + element.DisplayName.toString().replace(' ', '').toLowerCase();
         document.getElementById("members-content").appendChild(a);
     });
 }
